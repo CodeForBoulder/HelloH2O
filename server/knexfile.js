1a61ad47b0ed81e1524014cbcf63e84414a96821
Andrew Eskenazi <drewesk@gmail.com>
2017-10-04 23:07:43 -0600

    Adding migration files

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,44 +1,15 @@
-// Update with your config settings.
+const dotEnv = require('dotenv');
+dotEnv.config();
 
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    client: 'pg',
+    connection: "postgresql://localhost/waterblog"
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?ssl=true'
   }
-
 };
